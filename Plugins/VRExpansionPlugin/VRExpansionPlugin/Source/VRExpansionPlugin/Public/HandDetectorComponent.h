f213e076ce593338b4e5d9ff334c1b6608d8db90
ISILV125 <igonildo7@gmail.com>
2022-09-24 00:40:27 -0300

    Added pot mesh, flower pot BP, moisture sensor mesh and BP, watering can mesh, BP, particle water effect and logic to interact with pot

--- Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Public/HandDetectorComponent.h
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Public/HandDetectorComponent.h
@@ -24,7 +24,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-private:
+public:
 
 	UPROPERTY(EditAnywhere)
 		float TraceDistance = 30.f;
