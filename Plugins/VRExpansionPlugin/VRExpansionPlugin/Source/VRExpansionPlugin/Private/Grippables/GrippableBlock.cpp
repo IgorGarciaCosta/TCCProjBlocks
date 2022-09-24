8b2f98753c94e8259e09b1a60ca9ad16c760abb3
ISILV125 <igonildo7@gmail.com>
2022-09-24 11:53:29 -0300

    Added cpp logic to hand detection line tracing and added square sprite indicators

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Private/Grippables/GrippableBlock.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grippables/GrippableBlock.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+
+
+
+AGrippableBlock::AGrippableBlock(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AGrippableBlock::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGrippableBlock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	
+
+}
+
+void AGrippableBlock::MakeTrace()
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Your message"));
+	FVector Start = GetActorLocation();
+	FVector End = Start + GetActorForwardVector() * TraceDistance;
+	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	FHitResult HitResult;
+	bool HastHit = GetWorld()->LineTraceSingleByChannel(
+		HitResult,
+		Start,
+		End,
+		ECollisionChannel::ECC_WorldDynamic
+	);
+	if (HastHit) {
+		UPrimitiveComponent* Target = HitResult.GetComponent();
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Target->GetName());
+		if (Target->GetName() == ActorNameOpt1 || Target->GetName() == ActorNameOpt2) {
+			bTouchedTarget = true;
+			if (bTouchedPreviousState == false) {
+				bTouchedPreviousState = true;
+				//AGrippableStaticMeshActor* BlockActor = Cast<AGrippableStaticMeshActor>(GetOwner());
+				//UE_LOG(LogTemp, Warning, TEXT("HAND"));
+				//BlockActor->TriggerBlockEvent();
+				TriggerSensorObjectIn();
+			}
+		}
+		else {
+			bTouchedTarget = false;
+			if (bTouchedPreviousState == true) {
+				bTouchedPreviousState = false;
+			}
+		}
+	}
+	else {
+		bTouchedTarget = false;
+		if (bTouchedPreviousState == true) {
+			bTouchedPreviousState = false;
+		}
+	}
+}
---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Source/VRExpansionPlugin/Public/Grippables/GrippableBlock.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Grippables/GrippableStaticMeshActor.h"
+#include "GrippableBlock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VREXPANSIONPLUGIN_API AGrippableBlock : public AGrippableStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AGrippableBlock(const FObjectInitializer& ObjectInitializer);
+
+public:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		float TraceDistance = 30.f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FString ActorNameOpt1 = "SkeletalMeshComponent0";
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		FString ActorNameOpt2 = "GraspingHand_C_1";
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool bTouchedTarget = false;
+	UPROPERTY(BlueprintReadOnly)
+		bool bTouchedPreviousState = false;
+
+	UFUNCTION(BlueprintCallable)
+		void MakeTrace();
+	UFUNCTION(BlueprintImplementableEvent)
+		void TriggerSensorObjectIn();
+	UFUNCTION(BlueprintImplementableEvent)
+		void TriggerSensorObjectOut();
+};
